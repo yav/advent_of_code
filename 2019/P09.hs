c6764e58a8d715639bebb644faf5c45799487dd5
Iavor Diatchki <iavor.diatchki@gmail.com>
2019-12-12 14:48:56 -0800

    Problem 9

--- 2019/P09.hs
+++ 2019/P09.hs
@@ -7,12 +7,21 @@ import VM
 main :: String -> IO ()
 main txt =
   do putStrLn "Problem 09"
-     let txt = ex1
      mem <- parseProgram txt
+
      vm  <- newVM mem
+     writeChan (vmIn vm) 1
      runProgram vm
      drain vm
 
+     vm2  <- newVM mem
+     writeChan (vmIn vm2) 2
+     runProgram vm2
+     drain vm2
+
+
+
+ex1, ex2, ex3 :: String
 ex1 = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
 ex2 = "1102,34915192,34915192,7,4,7,99,0"
 ex3 = "104,1125899906842624,99"
